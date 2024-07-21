47504c13b29f962dddc88a2a4c000baa8ace4f74
JensenzhaoUT <Jensen.zhao@mail.utoronto.ca>
2024-07-21 16:05:24 -0400

    Changed run.bash

--- Docker/run.bash
+++ Docker/run.bash
@@ -2,7 +2,7 @@ xhost local:root
 XAUTH=/tmp/.docker.xauth
 docker run --rm -it \
     --name=rag_container\
-    --volume="/home/$USER//projects/Agentic_RAG_Pipeline_KnowledgeGraph:/app" \
+    --volume="/home/$USER//projects/CrewAI_RAG_Pipeline_KnowledgeGraph:/app" \
     --volume="/dev/bus/usb:/dev/bus/usb" \
     --volume="/tmp/.docker.xauth:/tmp/.docker.xauth:rw" \
     --gpus=all \
